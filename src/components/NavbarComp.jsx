1953e64d698e167e5dbe56884cb3755109990472
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-10-21 22:47:19 -0300

    cambio en navbar

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
+import { Context } from '../Context';
+import { NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Badge from 'react-bootstrap/Badge';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
-import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCartShopping,
@@ -13,15 +14,14 @@ import {
   faUserXmark,
   faTableColumns,
 } from '@fortawesome/free-solid-svg-icons';
-import { Context } from '../Context';
 
 export default function NavbarComp() {
   const { cart, formatNum, goToHome } = useContext(Context);
+
   const token = localStorage.getItem('token');
   const setActiveClass = ({ isActive }) =>
     isActive ? 'active' : 'td-none txt-violet';
 
-  console.log('cart: ', cart);
   const tot = cart.reduce((prev, { price, q }) => prev + price * q, 0);
 
   const logout = () => {
@@ -45,61 +45,7 @@ export default function NavbarComp() {
             />
           </span>
         </Navbar.Brand>
-        <Container className="text-end">
-          <NavLink className={setActiveClass} to="/dashboard">
-            <OverlayTrigger
-              placement="bottom"
-              overlay={<Tooltip id="tooltip-gz">Dashboard</Tooltip>}
-            >
-              <FontAwesomeIcon
-                className={token ? 'nav-icon-spc' : 'dn'}
-                icon={faTableColumns}
-                size="lg"
-              />
-            </OverlayTrigger>
-          </NavLink>
-
-          <NavLink to="/">
-            <OverlayTrigger
-              placement="bottom"
-              overlay={<Tooltip id="tooltip-gz">Cerrar Sesión</Tooltip>}
-            >
-              <FontAwesomeIcon
-                className={token ? 'nav-icon-spc' : 'dn'}
-                icon={faUserXmark}
-                style={{ color: '#e71861' }}
-                size="lg"
-                onClick={logout}
-              />
-            </OverlayTrigger>
-          </NavLink>
-
-          <NavLink className={setActiveClass} to="/login">
-            <OverlayTrigger
-              placement="bottom"
-              overlay={<Tooltip id="tooltip-gz">Login</Tooltip>}
-            >
-              <FontAwesomeIcon
-                className={'nav-icon-spc'}
-                icon={faUserCheck}
-                size="lg"
-              />
-            </OverlayTrigger>
-          </NavLink>
-          <span></span>
-          <NavLink className={setActiveClass} to="/Registro">
-            <OverlayTrigger
-              placement="bottom"
-              overlay={<Tooltip id="tooltip-gz">Registrarse</Tooltip>}
-            >
-              <FontAwesomeIcon
-                className="nav-icon-spc"
-                icon={faUserPlus}
-                size="lg"
-              />
-            </OverlayTrigger>
-          </NavLink>
-          <span></span>
+        <Container className="px-0 text-end">
           <NavLink className={setActiveClass} to="/Cart">
             <FontAwesomeIcon
               className="nav-icon-spc"
@@ -111,6 +57,61 @@ export default function NavbarComp() {
             </Badge>{' '}
             <strong>{formatNum(tot)}</strong>
           </NavLink>
+          <Container className="px-0">
+            <NavLink className={setActiveClass} to="/dashboard">
+              <OverlayTrigger
+                placement="bottom"
+                overlay={<Tooltip id="tooltip-gz">Dashboard</Tooltip>}
+              >
+                <FontAwesomeIcon
+                  className={token ? 'nav-icon-spc' : 'dn'}
+                  icon={faTableColumns}
+                  size="sm"
+                />
+              </OverlayTrigger>
+            </NavLink>
+
+            <NavLink to="/">
+              <OverlayTrigger
+                placement="bottom"
+                overlay={<Tooltip id="tooltip-gz">Cerrar Sesión</Tooltip>}
+              >
+                <FontAwesomeIcon
+                  className={token ? 'nav-icon-spc' : 'dn'}
+                  icon={faUserXmark}
+                  style={{ color: '#e71861' }}
+                  size="sm"
+                  onClick={logout}
+                />
+              </OverlayTrigger>
+            </NavLink>
+
+            <NavLink className={setActiveClass} to="/login">
+              <OverlayTrigger
+                placement="bottom"
+                overlay={<Tooltip id="tooltip-gz">Login</Tooltip>}
+              >
+                <FontAwesomeIcon
+                  className={'nav-icon-spc'}
+                  icon={faUserCheck}
+                  size="sm"
+                />
+              </OverlayTrigger>
+            </NavLink>
+            <span></span>
+            <NavLink className={setActiveClass} to="/Registro">
+              <OverlayTrigger
+                placement="bottom"
+                overlay={<Tooltip id="tooltip-gz">Registrarse</Tooltip>}
+              >
+                <FontAwesomeIcon
+                  className="nav-icon-spc"
+                  icon={faUserPlus}
+                  size="sm"
+                />
+              </OverlayTrigger>
+            </NavLink>
+          </Container>
         </Container>
       </Container>
     </Navbar>
